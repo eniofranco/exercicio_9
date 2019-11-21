b9f21ba6660f68750ff6aaf7247a0948dca96421
Enio Franco <eniovito@gmail.com>
2019-11-21 00:21:05 -0200

    exercicio_9.1a

--- src/TestaProduto.java
+++ src/TestaProduto.java
@@ -4,6 +4,8 @@ public class TestaProduto {
         Motor m1 = new Motor ("E2531", "Ford", "Forte", 4562.33, 160, 5200);
         Parafuso p1 = new Parafuso ("P745", "WEG", "Inox", 2.55, 7.88, 1.22);
         
+        System.out.println (m1.calcularPrecoFinal());
+        System.out.println (p1.calcularPrecoFinal());
         System.out.println (m1.imprimirDados());
         System.out.println (p1.imprimirDados());
         
